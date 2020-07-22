616318d2edae6d22aac27248ff91e2084b630b98
gongjian <gongjian@zycfcinc.com>
2020-07-22 10:41:24 +0800

    feat: point free模式

---
+++ FUNCTIONAL-PROGRAMMING/18-point-free.js
@@ -0,0 +1,6 @@
+// point free 模式
+// Hello     World => hello_world
+
+const fp = require('lodash/fp')
+const f = fp.flowRight(fp.replace(/\s+/g, '_') ,fp.toLower)
+console.log(f('Hello     World'))
