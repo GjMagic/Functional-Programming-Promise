e8c048158d2d122742cb53302ac3e363736f336f
gongjian <gongjian@zycfcinc.com>
2020-07-22 15:23:42 +0800

    feat: IO函子

---
+++ FP-FUNCTOR/04-io.js
@@ -0,0 +1,21 @@
+// IO 函子
+const fp = require('lodash/fp')
+class IO {
+  static of (value) {
+    return new IO(function () {
+      return value
+    })
+  }
+
+  constructor (fn) {
+    this._value = fn
+  }
+
+  map (fn) {
+    return new IO(fp.flowRight(fn, this._value))
+  }
+}
+
+let r = IO.of(process).map(p => p.execPath)
+
+console.log(r._value())
