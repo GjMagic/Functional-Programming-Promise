084b937d0a1a1b219f514de63665d55aa1d64d47
gongjian <gongjian@zycfcinc.com>
2020-07-24 14:57:23 +0800

    feat: Promise核心逻辑实现

--- PROMISE/01-promise.js
+++ PROMISE/01-promise.js
@@ -25,34 +25,79 @@ class Promise {
 
   reason = undefined
 
+  onFulfilled = []
+
+  onRejected = []
+
   resolve = (value) => {
     if (this.status !== PENDING) return;
     this.status = FULFILLED
     this.value = value
+    // this.onFulfilled && this.onFulfilled(this.value)
+    while (this.onFulfilled.length) this.onFulfilled.shift()(this.value)
   }
 
   reject = (reason) => {
     if (this.status !== PENDING) return;
     this.status = REJECTED
     this.reason = reason
+    // this.onRejected && this.onRejected(this.reason)
+    while (this.onRejected.length) this.onRejected.shift()(this.reason)
   }
 
   then (onFulfilled, onRejected) {
-    if (this.status === FULFILLED) {
-      onFulfilled(this.value)
-    } else if (this.status === REJECTED) {
-      onRejected(this.reason)
-    }
+    let promise2 = new Promise((resolve, reject) => {
+      if (this.status === FULFILLED) {
+        let x =  onFulfilled(this.value)
+        resolvePromise(x, resolve, reject)
+      } else if (this.status === REJECTED) {
+        onRejected(this.reason)
+      } else {
+        this.onFulfilled.push(onFulfilled)
+        this.onRejected.push(onRejected)
+      }
+    })
+    return promise2
   }
 }
 
+function resolvePromise (x, resolve, reject) {
+  if (x instanceof Promise) {
+    x.then(resolve, reject)
+  } else {
+    resolve(x)
+  }
+}
+
+promiseObj = () => {
+  return new Promise((resolve, reject) => {
+    resolve('promiseObj')
+  })
+}
+
 const promise = new Promise((resolve, reject) => {
+  /* setTimeout(() => {
+    resolve('成功')
+  }, 2000); */
   resolve('成功')
-  reject('失败')
+  // reject('失败')
+})
+
+promise.then((res) => {
+  console.log(res)
+  return promiseObj()
+}).then(res => {
+  console.log(res)
+})
+
+/* promise.then((res) => {
+  console.log(res)
+}, (err) => {
+  console.log(err)
 })
 
 promise.then((res) => {
   console.log(res)
 }, (err) => {
   console.log(err)
-})
+}) */
