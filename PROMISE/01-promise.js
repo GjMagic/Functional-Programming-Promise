99b99b76d7d97298afa83805b41b28d3cfc1e88c
gongjian <gongjian@zycfcinc.com>
2020-07-27 09:23:32 +0800

    feat: promise核心逻辑

--- PROMISE/01-promise.js
+++ PROMISE/01-promise.js
@@ -16,7 +16,11 @@ const REJECTED = 'rejected'
 
 class Promise {
   constructor (executor) {
-    executor(this.resolve, this.reject)
+    try {
+      executor(this.resolve, this.reject)
+    } catch (e) {
+      this.reject(e)
+    }
   }
 
   status = PENDING
@@ -34,7 +38,7 @@ class Promise {
     this.status = FULFILLED
     this.value = value
     // this.onFulfilled && this.onFulfilled(this.value)
-    while (this.onFulfilled.length) this.onFulfilled.shift()(this.value)
+    while (this.onFulfilled.length) this.onFulfilled.shift()()
   }
 
   reject = (reason) => {
@@ -42,26 +46,105 @@ class Promise {
     this.status = REJECTED
     this.reason = reason
     // this.onRejected && this.onRejected(this.reason)
-    while (this.onRejected.length) this.onRejected.shift()(this.reason)
+    while (this.onRejected.length) this.onRejected.shift()()
   }
 
   then (onFulfilled, onRejected) {
+    onFulfilled = onFulfilled ? onFulfilled : value => value;
+    onRejected = onRejected ? onRejected : reason => reason;
     let promise2 = new Promise((resolve, reject) => {
       if (this.status === FULFILLED) {
-        let x =  onFulfilled(this.value)
-        resolvePromise(x, resolve, reject)
+        setTimeout(() => {
+          try {
+            let x = onFulfilled(this.value)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0)
       } else if (this.status === REJECTED) {
-        onRejected(this.reason)
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0)
       } else {
-        this.onFulfilled.push(onFulfilled)
-        this.onRejected.push(onRejected)
+        this.onFulfilled.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0)
+        })
+        this.onRejected.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.value)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0)
+        })
       }
     })
     return promise2
   }
+
+  finally (callback) {
+    return this.then((res) => {
+      return Promise.resolve(callback()).then(() => res)
+    }, (err) => {
+      return Promise.resolve(callback()).then(() => err)
+    })
+  }
+
+  catch (onRejected) {
+    return this.then(undefined, onRejected)
+  }
+
+  static all (arr) {
+    let result = []
+    let index = 0
+
+    return new Promise((resolve, reject) => {
+      function addData (key, value) {
+        result[key] = value
+        index++
+        if (index === arr.length) {
+          resolve(result)
+        }
+      }
+
+      for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        if (element instanceof Promise) {
+          element.then(res => addData(i, arr[i]), err => reject(err))
+        } else {
+          addData(i, arr[i])
+        }
+      }
+    })
+  }
+
+  static resolve (value) {
+    if (value instanceof Promise) return value
+
+    return new Promise(resolve => resolve(value))
+  }
 }
 
-function resolvePromise (x, resolve, reject) {
+function resolvePromise (promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    return reject(new TypeError('TypeError: Chaining cycle detected for promise #<Promise>'))
+  }
+
   if (x instanceof Promise) {
     x.then(resolve, reject)
   } else {
@@ -79,16 +162,27 @@ const promise = new Promise((resolve, reject) => {
   /* setTimeout(() => {
     resolve('成功')
   }, 2000); */
-  resolve('成功')
-  // reject('失败')
+  // throw Error('executor error')
+  // resolve('成功')
+  reject('失败')
 })
 
-promise.then((res) => {
-  console.log(res)
-  return promiseObj()
-}).then(res => {
-  console.log(res)
+// promise.then().then().then(console.log, console.log)
+
+/* let p2 = promise.then((res) => {
+  console.log(res, '111')
+  // throw Error('then error')
+  return '111'
+}, (err) => {
+  console.log(err)
+  // throw Error('err error')
 })
+
+p2.then(res => {
+  console.log(res)
+}, err => {
+  console.log(err.message)
+}) */
 
 /* promise.then((res) => {
   console.log(res)
@@ -101,3 +195,34 @@ promise.then((res) => {
 }, (err) => {
   console.log(err)
 }) */
+
+function p1 () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // resolve('p1')
+      reject('失败')
+    }, 2000);
+  })
+}
+
+function p2 () {
+  return new Promise((resolve, reject) => {
+    resolve('成功')
+    // reject('失败')
+  })
+}
+
+/* Promise.all(['a', 'b', p1(), p2(), 'c']).then(res => {
+  console.log(res)
+}, (err) => {
+  console.log(err)
+}) */
+/* Promise.resolve('a').then(console.log)
+Promise.resolve(p1()).then(console.log) */
+
+p2().finally((res) => {
+  console.log('finally', res)
+  return p1()
+}).then(console.log, console.log)
+
+// p2().catch(console.log).then(console.log)
